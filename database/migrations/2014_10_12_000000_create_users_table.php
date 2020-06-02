8abc85bc7298cacc4b2cf93c2542e4f7a8545dff
Aryan Gomes <aryan.gomes@gmail.com>
2020-06-01 22:25:34 -0300

    add migration passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->string('streetAddress');
+            $table->string('streetAddressstreetAddress');
             $table->string('neighborhoodAddress');
             $table->string('numberAddress');
             $table->string('phoneNumber')->nullable();
