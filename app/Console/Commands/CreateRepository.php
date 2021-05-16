05a80181c284bfa64afa18e94a21dceb47e9ef85
Aryan Gomes <aryan.gomes@gmail.com>
2021-05-16 17:27:41 -0300

    Implementing command of create repository.

--- app/Console/Commands/CreateRepository.php
+++ app/Console/Commands/CreateRepository.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 
 class CreateRepository extends Command
 {
@@ -11,7 +12,7 @@ class CreateRepository extends Command
      *
      * @var string
      */
-    protected $signature = 'make:repository {classRepositoryName}';
+    protected $signature = 'make:repository {classRepositoryName} {--model=}';
 
     /**
      * The console command description.
@@ -20,7 +21,22 @@ class CreateRepository extends Command
      */
     protected $description = 'Create a Repository';
 
+    /** @var String $classRepositoryName classRepositoryName */
     private $classRepositoryName;
+
+    /** @var String $classModelName classModelName */
+    private $classModelName;
+
+    /** @var Filesystem $filesystem filesystem */
+    private $filesystem;
+
+    /** @var String $classRepositoryName classRepositoryName */
+    private static $pathRepositories = './app/Repositories';
+
+    private static $pathModels = './app/Models';
+
+    /** @var Boolean $createRepositoryWasSuccessfully createRepositoryWasSuccessfully */
+    private  $createRepositoryWasSuccessfully;
 
 
     /**
@@ -28,8 +44,11 @@ class CreateRepository extends Command
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Filesystem $filesystem)
     {
+
+        $this->createRepositoryWasSuccessfully = false;
+        $this->filesystem = $filesystem;
         parent::__construct();
     }
 
@@ -40,23 +59,32 @@ class CreateRepository extends Command
      */
     public function handle()
     {
+        $this->classModelName = $this->option('model');
+
         $this->generateNameRepositoryClass($this->argument('classRepositoryName'));
 
-        $pathRepositories = './app/Repositories';
 
-        $commandMakeDirectory = "mkdir {$pathRepositories}\n";
-        $commandTouchRepositoryFile = "touch {$pathRepositories}/{$this->classRepositoryName}.php\n";
+        $this->makeDirectoryRepositories();
+
+        $this->makeRepositoryClass();
+
+        $this->resultOfCommand();
+
+
+
+
+        /*   $commandTouchRepositoryFile = "touch {$this::$pathRepositories}/{$this->classRepositoryName}.php\n";
 
         $templateRepositoryClassFile = $this->generateClassContent();
 
-        $commandFillRepositoryClassFile = "echo $templateRepositoryClassFile > {$pathRepositories}/{$this->classRepositoryName}.php";
+        $commandFillRepositoryClassFile = "echo $templateRepositoryClassFile > {$this::$pathRepositories}/{$this->classRepositoryName}.php";
 
         $commandCreateRepository =   $commandMakeDirectory .
             $commandTouchRepositoryFile . $commandFillRepositoryClassFile;
 
-        exec($commandCreateRepository, $output);
+        exec($commandCreateRepository, $output); 
 
-        $this->info(implode(PHP_EOL, $output));
+        $this->info(implode(PHP_EOL, $output));*/
         //
     }
 
@@ -64,7 +92,41 @@ class CreateRepository extends Command
     private function generateClassContent()
     {
 
-        $classContent = "\"<?php\nnamespace App\Repositories;\nuse App\Repositories\Interfaces\AcquisitionRepositoryInterface;\nuse App\Repositories\ModelRepository;\nclass {$this->classRepositoryName} extends ModelRepository implements AcquisitionRepositoryInterface\n{\n}\"";
+        $classContent = "<?php\nnamespace App\Repositories;\nuse App\Repositories\Interfaces\AcquisitionRepositoryInterface;\nuse App\Repositories\ModelRepository;\nclass {$this->classRepositoryName} extends ModelRepository implements AcquisitionRepositoryInterface\n{\n}";
+
+        $linesHeaderContentClass = [
+            "<?php\n",
+            "namespace App\Repositories;\n",
+            "use App\Repositories\Interfaces\AcquisitionRepositoryInterface;\n",
+            "use App\Repositories\ModelRepository;\n",
+        ];
+
+        if (isset($this->classModelName)) {
+            array_push($linesHeaderContentClass, "use App\Models\\{$this->classModelName};\n");
+        }
+
+
+        $linesBodyContentClass = [
+
+            "class {$this->classRepositoryName} extends ModelRepository implements AcquisitionRepositoryInterface",
+            "{",
+            "/**",
+            "*",
+            "*",
+            "* @param {$this->argument('classRepositoryName')} {$this->generateNameRepositoryClassModel($this->argument('classRepositoryName'))}\n",
+            "*/",
+            "public function __construct({$this->argument('classRepositoryName')} \${$this->generateNameRepositoryClassModel($this->argument('classRepositoryName'))})\n",
+            "{",
+            "parent::__construct(\${$this->generateNameRepositoryClassModel($this->argument('classRepositoryName'))});\n",
+            "}",
+
+            "}\n",
+        ];
+        $linesContentClass = array_merge($linesHeaderContentClass, $linesBodyContentClass);
+        $classContent = "";
+        foreach ($linesContentClass as  $line) {
+            $classContent .= "{$line}\n";
+        }
 
         return $classContent;
     }
@@ -74,5 +136,68 @@ class CreateRepository extends Command
     {
         $nameRepositoryClass =  "{$nameClass}Repository";
         $this->classRepositoryName = $nameRepositoryClass;
+    }
+
+    private function generateNameRepositoryClassModel($nameClass)
+    {
+        $nameRepositoryClassModel =  lcfirst("{$nameClass}Model");
+        return  $nameRepositoryClassModel;
+    }
+
+    private function makeDirectoryRepositories()
+    {
+        $directoryRepositoriesExists = $this->filesystem->exists($this::$pathRepositories);
+        if (!$directoryRepositoriesExists) {
+            try {
+                $this->filesystem->makeDirectory($this::$pathRepositories);
+            } catch (\Exception $exception) {
+                $this->logErrorFromException($exception);
+            }
+        }
+    }
+
+    private function makeRepositoryClass()
+    {
+        $fullPathOfFileRepositoryClass = "{$this::$pathRepositories}/{$this->classRepositoryName}.php";
+
+        $repositoryClassExists = $this->filesystem->exists($fullPathOfFileRepositoryClass);
+        if (!$repositoryClassExists) {
+            try {
+
+                $this->filesystem->put($fullPathOfFileRepositoryClass, $this->generateClassContent());
+
+                $this->createRepositoryWasSuccessfully = true;
+            } catch (\Exception $exception) {
+
+                $this->logErrorFromException($exception);
+            }
+        } else {
+            $this->warn("Class already exists!");
+        }
+    }
+
+    private function logErrorFromException(\Exception $exception)
+    {
+        $this->warn($exception);
+        logger(
+            get_class($this),
+            [
+                'exception' => $exception
+            ]
+        );
+    }
+
+
+    private function resultOfCommand()
+    {
+        $resultOfCommand = "Not was possible create the Class Repository";
+
+        if ($this->createRepositoryWasSuccessfully) {
+
+            $resultOfCommand = "Class Repository was created successfully!";
+            $this->info($resultOfCommand);
+        } else {
+            $this->error($resultOfCommand);
+        }
     }
 }
--- app/Repositories/ClasseRepository.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Repositories;
-use App\Repositories\Interfaces\AcquisitionRepositoryInterface;
-use App\Repositories\ModelRepository;
-class ClasseRepository extends ModelRepository implements AcquisitionRepositoryInterface
-{
-}
